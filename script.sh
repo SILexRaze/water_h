6cbaac934d0c6bb812707205ece623bf7c891263
SILexRaze <fonne.victor@gmail.com>
2018-12-28 11:28:08 +0100

    add some security to parse temp and mesure values

--- script.sh
+++ script.sh
@@ -6,10 +6,11 @@ temp=$(echo "$(cat /var/www/html/wthr.json | tr ',' '\n' | tr '{' '\n' | grep -E
 hum=$(cat /var/www/html/wthr.json | tr ',' '\n' | tr '{' '\n' | grep -E '\"humidity\"' | awk -F ":" '{print$2}')
 mesure=$(cat /var/www/html/tmp.json)
 date=$(date "+%d/%m/%Y/%H/%M" 2> /dev/null)
+ent=$(echo "$temp" | awk -F "." '{print$1}')
 rm -f /var/www/html/tmp.json /var/www/html/wthr.json /var/www/html/teste
-if [ $mesure -ne 0 ]
+if [ $mesure -le 200 -a $mesure -gt 10 -a $ent -le 40 -a $ent -ge -25 ]
 then
-	cat /var/www/html/data.json | sed '/^\]$/d' | sed 's/\}$/\},/g' > /var/www/html/data2.json
+	cat /var/www/html/data.json | sed '/^\]$/d' | sed 's/\}$/\},/g' > /var/www/html/data2.json	
 	mv /var/www/html/data2.json /var/www/html/data.json
 	echo "\t{\n\t\t\"mesure\":$mesure,\n\t\t\"temp\":$temp,\n\t\t\"humidity\":$hum,\n\t\t\"date\":\"$date\"\n\t}\n]" >> /var/www/html/data.json
 fi
