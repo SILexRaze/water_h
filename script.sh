a77f6b45dd1ad899712dfc0432d06ddbdd8cd08a
SILexRaze <fonne.victor@gmail.com>
2018-12-28 15:47:08 +0100

    change color of update message

--- script.sh
+++ script.sh
@@ -3,12 +3,12 @@ curl http://api.openweathermap.org/data/2.5/weather\?id\=3003496\&appid\=7b61461
 curl http://192.168.1.133:4200/ -o /var/www/html/teste 2> /dev/null
 cat /var/www/html/teste | tr '(' '\n' | grep -E "\(*.*cm" | cut -d ',' -f 1 | cut -d '.' -f 1 > /var/www/html/tmp.json
 temp=$(echo "$(cat /var/www/html/wthr.json | tr ',' '\n' | tr '{' '\n' | grep -E '\"temp\"' | awk -F ":" '{print$2}') 273.15" | awk '{printf "%f", $1 - $2}')
-hum=$(cat /var/www/html/wthr.json | tr ',' '\n' | tr '{' '\n' | grep -E '\"humidity\"' | awk -F ":" '{print$2}')
+hum=$(cat /var/www/html/wthr.json | tr ',' '\n' | tr '{' '\n' | grep -E '\"pressure\"' | awk -F ":" '{print$2}')
 mesure=$(cat /var/www/html/tmp.json)
 date=$(date "+%d/%m/%Y/%H/%M" 2> /dev/null)
 ent=$(echo "$temp" | awk -F "." '{print$1}')
 rm -f /var/www/html/tmp.json /var/www/html/wthr.json /var/www/html/teste
-if [ $mesure -le 200 -a $mesure -gt 10 -a $ent -le 40 -a $ent -ge -25 ]
+if [ $mesure -le 120 -a $mesure -gt 10 -a $ent -le 40 -a $ent -ge -25 ]
 then
 	cat /var/www/html/data.json | sed '/^\]$/d' | sed 's/\}$/\},/g' > /var/www/html/data2.json	
 	mv /var/www/html/data2.json /var/www/html/data.json
