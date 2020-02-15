d521deb6074eb152a479ff1f1507ae96e033ae1b
SILexRaze <fonne.victor@gmail.com>
2020-02-15 12:36:55 +0100

    maj before migration

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl http://api.openweathermap.org/data/2.5/weather\?id\=3003496\&appid\=7b61461b16a85d0d47cdcd7d41857d68 -o /var/www/html/wthr.json 2> /dev/null
-curl http://192.168.1.133:4200/ -o /var/www/html/teste 2> /dev/null
+curl http://192.168.1.130:4200/ -o /var/www/html/teste 2> /dev/null
 cat /var/www/html/teste > /var/www/html/tmp.json
 temp=$(echo "$(cat /var/www/html/wthr.json | tr ',' '\n' | tr '{' '\n' | grep -E '\"temp\"' | awk -F ":" '{print$2}') 273.15" | awk '{printf "%f", $1 - $2}')
 pres=$(cat /var/www/html/wthr.json | tr ',' '\n' | tr '{' '\n' | grep -E '\"pressure\"' | awk -F ":" '{print$2}')
@@ -13,5 +13,5 @@ then
 	cat /var/www/html/data.json | sed '/^\]$/d' | sed 's/\}$/\},/g' > /var/www/html/data2.json	
 	mv /var/www/html/data2.json /var/www/html/data.json
 	echo "\t{\n\t\t\"mesure\":$mesure,\n\t\t\"temp\":$temp,\n\t\t\"pressure\":$pres,\n\t\t\"date\":\"$date\"\n\t}\n]" >> /var/www/html/data.json
-	echo "\t{\n\t\t\"mesure\":$mesure,\n\t\t\"temp\":$temp,\n\t\t\"pressure\":$pres,\n\t\t\"date\":\"$date\"\n\t}\n]" > /var/www/html/last.json
+	echo "{\"mesure\":$mesure,\"temp\":$temp,\"pressure\":$pres,\"date\":\"$date\"}" > /var/www/html/last.json
 fi
